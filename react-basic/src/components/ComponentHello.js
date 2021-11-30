858342af4994536045fa0b9e44baee632fb13664
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-11-30 14:20:04 +0700

    add ComponentHello

---
+++ react-basic/src/components/ComponentHello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ComponentHello extends React.Component{
+    render(){
+        return(
+            <div>Hello world - I'm ComponentHello</div>
+        );
+    }
+}
+export default ComponentHello;
