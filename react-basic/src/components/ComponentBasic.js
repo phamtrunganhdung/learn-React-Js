cb43c153e86f52f9f2a3e28b402a8f801165edb9
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-11-30 16:03:49 +0700

    handling event

---
+++ react-basic/src/components/ComponentBasic.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class ComponentBasic extends React.Component{
+    state = {
+        name: 'ComponentHello',
+        status: 'Happy',
+        write: 'Write every thing'
+    }
+    showName = ()=>{
+        alert('My name is '+this.state.name);
+    }
+    showStatus = ()=>{
+        alert ('I am so '+this.state.status);
+    }
+    changeContentInput = (event)=>{
+        this.setState({
+            write: event.target.value
+        })
+    }
+    render(){
+        return(
+            <>            
+                <div className="hello-world">
+                    <h4>Hello world</h4>
+                </div>
+                <div className="handling-event">
+                   <button onClick={()=> this.showName()}>Alert name</button>
+                   <button onClick={()=> this.showStatus()}>Alert status</button>
+                   <br/>
+                   <input type="text" value={this.state.write} onChange={(event)=> this.changeContentInput(event)}></input>
+                   <p>Write: {this.state.write}</p>
+                </div>
+            </>
+        );
+    }
+}
+export default ComponentBasic;
--- react-basic/src/views/App.js
+++ react-basic/src/views/App.js
@@ -1,14 +1,14 @@
 import logo from '../logo.svg';
 import '../styles/App.css';
-import '../components/ComponentHello';
-import ComponentHello from '../components/ComponentHello';
+import '../components/ComponentBasic';
+import ComponentBasic from '../components/ComponentBasic';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ComponentHello />
+        <ComponentBasic />
       </header>
     </div>
   );
