69ca8bbf8c26dfce68af06ce0b22f07a134a0434
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-12-01 11:17:19 +0700

    cmt cpnbasic

--- react-basic/src/views/App.js
+++ react-basic/src/views/App.js
@@ -1,14 +1,14 @@
 import logo from '../logo.svg';
 import '../styles/App.css';
 import '../components/ComponentBasic';
-import ComponentBasic from '../components/ComponentBasic';
+// import ComponentBasic from '../components/ComponentBasic';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ComponentBasic />
+       {/* <ComponentBasic /> */}
       </header>
     </div>
   );
