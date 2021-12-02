edb69efb20b901d8a7f189136601c36895ad747e
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-12-02 22:03:59 +0700

    add todo & add notification

--- todo-app-pro/src/index.js
+++ todo-app-pro/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './views/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- todo-app-pro/src/views/App.js
+++ todo-app-pro/src/views/App.js
@@ -1,15 +1,32 @@
 import logo from '../logo.svg';
 import '../styles/App.scss';
+import ListTodo from '../components/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TODO-APP
-        </p>
+        <h2 className="title">
+          Todo app with React Js
+        </h2>
+        <ListTodo />
       </header>
+      <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+/>
+{toast.success()}
     </div>
   );
 }
