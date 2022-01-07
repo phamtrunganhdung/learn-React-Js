4ae7fcd6fe5fd0a0558bc724af2e5c0dd9fb1634
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-01-07 18:32:20 +0700

    import redux

--- todo-app-pro/src/index.js
+++ todo-app-pro/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './views/App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+import App from "./views/App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+
+const reduxStore = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
---
+++ todo-app-pro/src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+  users: [
+    { id: 1, name: "Dung" },
+    { id: 2, name: "Bo" },
+  ],
+};
+const rootReducer = (state = initState, actions) => {
+  return state;
+};
+
+export default rootReducer;
