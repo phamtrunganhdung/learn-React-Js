f474ba2cf80b9d696deeff4807d9fa9bc3ce0f3b
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-01-07 20:56:44 +0700

    use actions redux

--- todo-app-pro/src/components/Home.js
+++ todo-app-pro/src/components/Home.js
@@ -4,9 +4,15 @@ import { withRouter } from "react-router";
 import { connect } from "react-redux";
 
 class Home extends React.Component {
+  handleDeleteNickName = (nickname) => {
+    this.props.deleteNicknameRedux(nickname);
+  };
+  handleAddNewNickName = () => {
+    this.props.addNewNickName();
+  };
   render() {
-    console.log("check data: ", this.props.data);
-    let listLinks = this.props.data;
+    let listLinks = this.props.links;
+    let listNickNames = this.props.nickName;
     return (
       <>
         <h4 style={{ color: "#1fc2c2" }}>My many links</h4>
@@ -21,6 +27,25 @@ class Home extends React.Component {
               );
             })}
         </div>
+        <h4 style={{ color: "#1fc2c2" }}>My nick name</h4>
+        <div>
+          {listNickNames &&
+            listNickNames.length > 0 &&
+            listNickNames.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - {item.name} -{" "}
+                  <span onClick={() => this.handleDeleteNickName(item)}>x</span>
+                </div>
+              );
+            })}
+          <button
+            className="btn btn-success"
+            onClick={() => this.handleAddNewNickName()}
+          >
+            Add random nickname
+          </button>
+        </div>
       </>
     );
   }
@@ -28,8 +53,23 @@ class Home extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    data: state.manyLinks,
+    links: state.manyLinks,
+    nickName: state.nickName,
   };
 };
 
-export default connect(mapStateToProps)(withRouter(Home));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteNicknameRedux: (nicknameDelete) =>
+      dispatch({
+        type: "DELETE_NICKNAME",
+        payload: nicknameDelete,
+      }),
+    addNewNickName: () =>
+      dispatch({
+        type: "ADD_NICKNAME",
+      }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Home));
--- todo-app-pro/src/store/reducers/rootReducer.js
+++ todo-app-pro/src/store/reducers/rootReducer.js
@@ -11,14 +11,45 @@ const initState = {
       link: "https://www.facebook.com/profile.php?id=100009744435194",
     },
     {
-      id: 2,
+      id: 3,
       title: "Youtube",
       link: "https://www.youtube.com/channel/UCdQgYnUF3VuT0LlUoxZeC5g",
     },
   ],
+  nickName: [
+    {
+      id: 1,
+      name: "Bo",
+    },
+    {
+      id: 2,
+      name: "Dun",
+    },
+    {
+      id: 3,
+      name: "Dun Dun",
+    },
+  ],
 };
-const rootReducer = (state = initState, actions) => {
-  return state;
+const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "DELETE_NICKNAME":
+      let nickName = state.nickName;
+      nickName = nickName.filter((item) => item.id !== action.payload.id);
+      return {
+        ...state,
+        nickName,
+      };
+    case "ADD_NICKNAME":
+      let id = Math.floor(Math.random() * 10000);
+      let newNickName = { id: id, name: `random - ${id}` };
+      return {
+        ...state,
+        nickName: [...state.nickName, newNickName],
+      };
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
