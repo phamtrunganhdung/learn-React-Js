62819b0c060faac0df85f26008a56312a1ebf9ae
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-01-07 19:57:57 +0700

    render reduxData

--- todo-app-pro/src/components/Home.js
+++ todo-app-pro/src/components/Home.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "../styles/Home.scss";
 import { withRouter } from "react-router";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
   render() {
+    console.log("check data: ", this.props.data);
+    let listLinks = this.props.data;
     return (
       <>
-        <a href="https://www.github.com/phamtrunganhdung/learn-React-Js">
-          Go to my github
-        </a>
+        <h4 style={{ color: "#1fc2c2" }}>My many links</h4>
+        <div>
+          {listLinks &&
+            listLinks.length > 0 &&
+            listLinks.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - <a href={item.link}>{item.title}</a>
+                </div>
+              );
+            })}
+        </div>
       </>
     );
   }
 }
 
-export default withRouter(Home);
+const mapStateToProps = (state) => {
+  return {
+    data: state.manyLinks,
+  };
+};
+
+export default connect(mapStateToProps)(withRouter(Home));
--- todo-app-pro/src/store/reducers/rootReducer.js
+++ todo-app-pro/src/store/reducers/rootReducer.js
@@ -1,7 +1,20 @@
 const initState = {
-  users: [
-    { id: 1, name: "Dung" },
-    { id: 2, name: "Bo" },
+  manyLinks: [
+    {
+      id: 1,
+      title: "Github",
+      link: "https://www.github.com/phamtrunganhdung",
+    },
+    {
+      id: 2,
+      title: "Facebook",
+      link: "https://www.facebook.com/profile.php?id=100009744435194",
+    },
+    {
+      id: 2,
+      title: "Youtube",
+      link: "https://www.youtube.com/channel/UCdQgYnUF3VuT0LlUoxZeC5g",
+    },
   ],
 };
 const rootReducer = (state = initState, actions) => {
